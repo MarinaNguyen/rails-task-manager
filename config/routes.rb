b009b1c1c70dfcc1c9506f44529f9d43b4a843fa
Marina Nguyen <nguyenmarina3@gmail.com>
2023-11-28 14:24:16 +0100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,21 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "tasks/new", to: "tasks#new"
+  # get "tasks/new", to: "tasks#new"
 
-  post "tasks", to: "tasks#create"
+  # post "tasks", to: "tasks#create"
 
-  get "tasks", to: "tasks#index"
+  # get "tasks", to: "tasks#index"
 
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id", to: "tasks#show", as: :task
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
 
-  patch "tasks/:id", to: "tasks#update"
+  # patch "tasks/:id", to: "tasks#update"
 
-  delete "tasks/:id", to:"tasks#destroy"
+  # delete "tasks/:id", to:"tasks#destroy"
+
+  resources :tasks
 
   # Defines the root path route ("/")
   # root "posts#index"
